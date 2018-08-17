2e7bb7e58647be7f86179184dc404dbd67b60213
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-08-17 15:04:20 +0700

    initial project

---
+++ src/main/java/com/bcorpse/springcomponent/SpringComponentApplication.java
@@ -0,0 +1,12 @@
+package com.bcorpse.springcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComponentApplication.class, args);
+	}
+}
